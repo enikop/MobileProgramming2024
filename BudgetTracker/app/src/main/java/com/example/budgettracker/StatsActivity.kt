a1bdf14ba9ac8aebb4ddfa3df6e2f7d633830cf5
enikop <90562775+enikop@users.noreply.github.com>
2024-11-12 15:22:05 +0100

    Design and currency selection for Stats

--- BudgetTracker/app/src/main/java/com/example/budgettracker/StatsActivity.kt
+++ BudgetTracker/app/src/main/java/com/example/budgettracker/StatsActivity.kt
@@ -3,7 +3,11 @@ package com.example.budgettracker
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.budgettracker.database.AppDatabase
 import com.example.budgettracker.model.Currency
 import com.example.budgettracker.model.ExchangeResult
@@ -25,47 +29,70 @@ import java.time.format.TextStyle
 import java.util.Locale
 
 class StatsActivity: AppCompatActivity(), RatesCallback {
-    private lateinit var result: ExchangeResult
-    private lateinit var spendingsData: List<Float>
+    private var result: ExchangeResult? = null
+    private lateinit var spendingData: List<Float>
     private lateinit var savingsData: List<Float>
     private lateinit var labels: List<String>
 
+    private var monthsShown = 12
+    private var chartTextSize = 15f
     private var exchangeService = ExchangeService()
     private var monthFormatter = DateTimeFormatter.ofPattern("yyyy-MM")
+    private var selectedCurrency = Currency.EUR
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stats)
         setSupportActionBar(toolbar)
+        barChart.setNoDataText("Loading diagram...")
+        barChart.setNoDataTextColor(Color.BLACK)
         btnMain.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-        exchangeService.getRates(this)
+        val currencies = Currency.values().map { it.name }
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, currencies)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val selectionPosition = adapter.getPosition(selectedCurrency.name)
+        spDiagramCurrency.adapter = adapter
+        spDiagramCurrency.setSelection(selectionPosition)
+        spDiagramCurrency.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                if(result == null) exchangeService.getRates(this@StatsActivity)
+                else {
+                    val selectedCurrencyName = spDiagramCurrency.selectedItem as String
+                    selectedCurrency = Currency.valueOf(selectedCurrencyName)
+                    initTransactions()
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // Optional: handle the case where no item is selected, if needed
+            }
+        }
     }
 
     private fun initTransactions() {
         val dbThread = Thread {
+            //Last year
             val today = LocalDate.now()
-            val months = (2 downTo 0).map {
+            val months = (monthsShown-1 downTo 0).map {
                 today.minusMonths(it.toLong()).format(monthFormatter)
             }
-            labels = (2 downTo 0).map {
+            labels = (monthsShown-1 downTo 0).map {
                 today.month.minus(it.toLong()).getDisplayName(TextStyle.SHORT, Locale.getDefault())
             }
-            val transactions1 = AppDatabase.getInstance(this).transactionDao().findTransactionsForMonth(months[0])
-            val transactions2 = AppDatabase.getInstance(this).transactionDao().findTransactionsForMonth(months[1])
-            val transactions3 = AppDatabase.getInstance(this).transactionDao().findTransactionsForMonth(months[2])
+            val stats = mutableListOf<Pair<Float, Float>>()
+            for(i in 0 until monthsShown) {
+                val transactions = AppDatabase.getInstance(this).transactionDao().findTransactionsForMonth(months[i])
+                stats.add(calculateMonthlyStat(transactions, selectedCurrency))
+            }
 
-            val stat1 = calculateMonthlyStat(transactions1, Currency.EUR)
-            val stat2 = calculateMonthlyStat(transactions2, Currency.EUR)
-            val stat3 = calculateMonthlyStat(transactions3, Currency.EUR)
-
-            spendingsData = listOf(stat1.second, stat2.second, stat3.second)
-            savingsData = listOf(stat1.first, stat2.first, stat3.first)
+            spendingData = stats.map{ it.second }
+            savingsData = stats.map{ it.first }
 
             runOnUiThread{
-                setupChart();
+                setupChart()
             }
         }
         dbThread.start()
@@ -73,49 +100,65 @@ class StatsActivity: AppCompatActivity(), RatesCallback {
 
     private fun setupChart() {
 
-        // Bar entries for each data point
-        val spendingEntries = spendingsData.mapIndexed { index, value -> BarEntry(index.toFloat()*1.25f, value) }
-        val savingEntries = savingsData.mapIndexed { index, value -> BarEntry(index.toFloat()*1.25f, value) }
+        val spendingEntries = spendingData.mapIndexed { index, value -> BarEntry(index.toFloat(), value) }
+        val savingEntries = savingsData.mapIndexed { index, value -> BarEntry(index.toFloat(), value) }
 
-        // Data sets for spendings (red) and savings (green)
-        val spendingDataSet = BarDataSet(spendingEntries, "Spendings (EUR)").apply { color = Color.RED; valueTextSize = 12f}
-        val savingDataSet = BarDataSet(savingEntries, "Savings (EUR)").apply { color = Color.GREEN; valueTextSize = 12f}
+
+        val spendingDataSet = BarDataSet(spendingEntries, "Spending ("+selectedCurrency.name+")").apply {
+            color = ContextCompat.getColor(this@StatsActivity, R.color.spendingBarColour)
+            valueTextSize = chartTextSize
+        }
+        val savingDataSet = BarDataSet(savingEntries, "Income ("+selectedCurrency.name+")").apply {
+            color = ContextCompat.getColor(this@StatsActivity, R.color.incomeBarColour)
+            valueTextSize = chartTextSize
+        }
 
         // Group the data sets
         val data = BarData(spendingDataSet, savingDataSet)
-        data.barWidth = 0.3f // Width of each individual bar
+        data.barWidth = 0.3f
 
         barChart.data = data
-        barChart.description.isEnabled = false // Hide the description
+        barChart.description.isEnabled = false
 
-        barChart.legend.textSize = 14f
+        //Legend format
+        barChart.legend.textSize = chartTextSize * 1.2f
         barChart.legend.verticalAlignment = Legend.LegendVerticalAlignment.TOP
-        barChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.CENTER
+        barChart.legend.horizontalAlignment = Legend.LegendHorizontalAlignment.RIGHT
         barChart.legend.orientation = Legend.LegendOrientation.HORIZONTAL
         barChart.legend.yOffset = 20f
         barChart.setExtraOffsets(0f, 0f, 0f, 10f)
 
-        // Set X-axis labels and format
+        // Enable scaling and dragging
+        barChart.setScaleEnabled(true)
+        barChart.isDragEnabled = true
+
+        // X-axis labels and format
         val xAxis = barChart.xAxis
-        xAxis.textSize = 14f
+        xAxis.textSize = chartTextSize * 1.2f
         xAxis.valueFormatter = IndexAxisValueFormatter(labels)
         xAxis.position = XAxis.XAxisPosition.BOTTOM
         xAxis.setDrawGridLines(false)
         xAxis.axisMinimum = 0.0f
+        xAxis.axisMaximum = monthsShown.toFloat()
         xAxis.granularity = 1.0f
         xAxis.setCenterAxisLabels(true)
+        barChart.setVisibleXRangeMinimum(3f)
+        barChart.setVisibleXRangeMaximum(3f)
 
-        // Configure Y-axis
+        // Y-axis format
         val leftAxis = barChart.axisLeft
-        leftAxis.textSize = 14f
+        leftAxis.textSize = chartTextSize * 1.2f
         leftAxis.axisMinimum = 0f // Start Y-axis at zero
         barChart.axisRight.isEnabled = false // Disable right Y-axis
 
-        // Adjust the spacing between bars (group spacing)
+        // Spacing between bars
         barChart.groupBars(0.0f, 0.4f, 0f)
 
-        // Refresh chart with new data
         barChart.invalidate()
+
+        //Scroll to this month
+        val lastIndex = savingEntries.size
+        barChart.moveViewToX(lastIndex.toFloat() - 1)
     }
 
     private fun convertAmount(amount: Double, fromCurrency: Currency, toCurrency: Currency, rates: Rates): Double {
@@ -146,6 +189,6 @@ class StatsActivity: AppCompatActivity(), RatesCallback {
     }
 
     override fun onError(error: Throwable) {
-        //TODO
+        barChart.setNoDataText("The diagram could not access the current exchange rates.")
     }
 }
